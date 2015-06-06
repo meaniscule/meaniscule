1c9b48f2accaa9e6e5374ae5f0a658cc4701ff24
Ash Ryan <ash@ashryanbeats.com>
2015-06-05 22:52:31 -0400

    Now seeding with URLs for repos.

--- generated/seed.js
+++ generated/seed.js
@@ -8,32 +8,48 @@ var chalk = require('chalk');
 
 var moduleNamesArray;
 
-startDb.then(function(){
+startDb.then(function() {
+  // Get all module names
   fs.readdirAsync(__dirname + '/node_modules/')
+    // Get package.json content for all modules
     .then(function(moduleNames) {
       moduleNamesArray = moduleNames.filter(function(e) {
         return e !== ".bin";
       });
       
-      moduleNamesArray.forEach(function(e) {
-        Nodemodule.create({title: e})
+      return Promise.all(moduleNamesArray.map(function(module){
+        return fs.readFileAsync(path.join(__dirname, '/node_modules/' + module + '/package.json'), {'encoding': 'utf8'});
+      }));
+    })
+    // Parse json
+    .then(function(files) {
+      return Promise.all(files.map(function(file){
+        return JSON.parse(file);
+      }));
+    })
+    // Get repository urls
+    .then(function(parsed) {
+      return Promise.all(parsed.map(function(obj) {
+        return obj.repository.url;
+      }));
+    })
+    // Create model-compatible objects
+    .then(function(repos) {
+      return Promise.all(moduleNamesArray.map(function(e, i) {
+        return {
+          title: e,
+          repoUrl: repos[i]
+        };
+      }));
+    })
+    // Write to the db and exit
+    .then(function(moduleObjects) {
+      moduleObjects.forEach(function(e) {
+        Nodemodule.create(e)
           .then(function(){
             console.log(chalk.green('Database seeded. Goodbye!'));
             process.exit(1);
           });
       });
-
-      //return fs.readFileAsync(path.join(__dirname, '/node_modules/' + 'npm' + '/package.json'), {'encoding': 'utf8'});
-
-      // return moduleNamesArray.map(function(e) {
-      //   if (e !== '.bin') {
-      //     return fs.readFileAsync(path.join(__dirname, '/node_modules/' + e + '/package.json'));
-      //   }
-      // });
-      // return Promise.all(moduleNamesArray.map(function(module){
-      //   if (module !== '.bin') {
-      //     return fs.readFileAsync(path.join(__dirname, '/node_modules/' + module + '/package.json'), {'encoding': 'utf8'});
-      //   }
-      // }));
     });
 });
