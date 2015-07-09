763172d3600fb7de79ebd80a753dd6309e15075e
Ash Ryan <ash@ashryanbeats.com>
2015-07-09 10:13:05 -0400

    Db dependency removed from start-nodb.js.

--- generated/server/start-nodb.js
+++ generated/server/start-nodb.js
@@ -3,22 +3,15 @@ require('babel/register');
 
 var chalk = require('chalk');
 
-// Returns a promise from ./db/index.js
-var startDb = require('./db');
 var port = (process.env.PORT || 4545);
-var app;
-  
 // Start the server
-startDb
-  .then(function() {
-    app = require('./app');
-    app.listen(port, function() {
-      console.log('The server is listening on port', chalk.green.bold(port), 'and loves you very much.');
-    });
-  })
-  .catch(function(err) {
-    console.log('Problem starting up!', chalk.red(err.message));
-    console.log('I\'m out!');
-    process.kill(1);
-  });
-
+var app = require('./app');
+    
+app.listen(port, function() {
+  console.log('The server is listening on port', chalk.green.bold(port), 'and loves you very much.');
+})
+.catch(function(err) {
+  console.log('Problem starting up!', chalk.red(err.message));
+  console.log('I\'m out!');
+  process.kill(1);
+});
