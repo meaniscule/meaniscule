a4d7d09bb8637e0d239fa44afde4acfebeb6ee33
Ash Ryan <ash@ashryanbeats.com>
2015-07-10 17:57:37 -0400

    Server patch for nodb.

--- generated/server/start.nodb.js
+++ generated/server/start.nodb.js
@@ -2,14 +2,18 @@
 require('babel/register');
 
 var chalk = require('chalk');
+var Promise = require('bluebird');
 
 var port = (process.env.PORT || 4545);
 // Start the server
 var app = require('./app');
-    
-app.listen(port, function() {
-  console.log('The server is listening on port', chalk.green.bold(port), 'and loves you very much.');
-})
+
+(new Promise(function(resolve, reject) {
+  app.listen(port, function() {
+    console.log('The server is listening on port', chalk.green.bold(port), 'and loves you very much.');
+    return resolve();
+  });
+}))    
 .catch(function(err) {
   console.log('Problem starting up!', chalk.red(err.message));
   console.log('I\'m out!');
