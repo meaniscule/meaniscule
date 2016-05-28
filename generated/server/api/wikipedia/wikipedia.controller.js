cbc78422a11049c945cf259fa078708a385d9d19
Ash <ash@ashryanbeats.com>
2016-05-28 17:42:46 -0400

    Getting random article from Wikipedia API.

--- generated/server/api/wikipedia/wikipedia.controller.js
+++ generated/server/api/wikipedia/wikipedia.controller.js
@@ -1,7 +1,23 @@
+'use strict';
+
+const request = require('request-promise');
+
 module.exports = {
   random: _random 
 }
 
 function _random(req, res, next) {
-  res.send('random');
+  let num = req.query.num_things || 1;
+
+  let options = {
+    uri: 'https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=0&grnlimit=' + num + '&format=json'
+  }
+
+  request(options)
+    .then(function(data) {
+      data = JSON.parse(data);
+      let pages = data.query.pages;
+
+      res.send(pages);
+    });
 }
